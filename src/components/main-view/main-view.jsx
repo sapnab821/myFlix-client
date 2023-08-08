539891cc78d27efbea87ca519e80ec2c4630a861
Sapna <sapnabolikal@gmail.com>
2023-08-08 10:24:18 -0400

    react forms

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,14 +2,28 @@ import { useState, useEffect } from "react";
 
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser= JSON.parse(localStorage.getItem("user"));
+    const storedToken= localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
 
     const [selectedMovies, setSelectedMovies] = useState(null);
+    const [user, setUser] = useState(storedUser? storedUser: null);
+
+    const [token, setToken] = useState(storedToken? storedToken: null);
 
     useEffect(() => {
-        fetch("https://sbmovie-flix-81059d891de6.herokuapp.com/movies")
+        if(!token){
+            return;
+        }
+
+        fetch("https://sbmovie-flix-81059d891de6.herokuapp.com/movies", 
+        {
+            headers: {Authorization: 'Bearer ${token}'}
+        })
             .then((response) => response.json())
             .then((data) => {
                 const moviesFromApi = data.map((movie) => {
@@ -28,7 +42,24 @@ export const MainView = () => {
                 });
                 setMovies(moviesFromApi);
             });
-    }, []);
+    }, [token]);
+
+   
+
+    if (!user) {
+        return (
+        <>
+        <LoginView onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+        }}
+         />
+         or
+         <SignupView />
+         </>
+      );
+    }
+
 
     if (movies.length === 0) {
         return <div>The List is empty!</div>
@@ -43,8 +74,12 @@ export const MainView = () => {
         );
     }
 
+    
+
     return (
         <div>
+        <button onClick = {() => {setUser(null); setToken(null);
+            localStorage.clear();}}>Logout</button>
             {movies.map((movie) => (
                 <MovieCard 
                             key={movie._id}
@@ -57,4 +92,6 @@ export const MainView = () => {
             ))}
         </div>
     );
+
+    
 } 
